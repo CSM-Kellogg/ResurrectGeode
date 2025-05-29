d2283c57849e09b168a24ee8ec25c62b49690580
LiamKellogg <kellogg@mines.edu>
2025-05-29 13:37:36 -0600

    Added the scheduler page (boiler plate)
    Just the initial push, working on TODO now

--- popup.js
+++ popup.js
@@ -1,18 +1,3 @@
-// document.getElementById("RetrievePage").addEventListener("click", () => {
-//   chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-//       chrome.scripting.executeScript({
-//           target: { tabId: tabs[0].id },
-//           function: getPageText
-//       }, (result) => {
-//           if (result && result[0]) {
-//               alert("Page content: " + result[0].result.substring(0, 100) + "...");
-//               saveTextToFile(result[0].result);
-//           }
-//       });
-//   });
-// });
-
-
 // The path that this executes from: https://reg-prod.mines.elluciancloud.com:8118/StudentRegistrationSsb/ssb/classSearch/classSearch
 document.getElementById("DownloadCatalog").addEventListener("click", () => {
   chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
@@ -23,20 +8,7 @@ document.getElementById("DownloadCatalog").addEventListener("click", () => {
   });
 });
 
-// Gets the main body of the web page (may need tweaking)
-function getPageText() {
-  return document.body.innerText;
-}
-
-// Saves some list to a file
-function saveTextToFile(text) {
-  const blob = new Blob([text], { type: "text/plain" });
-  const url = URL.createObjectURL(blob);
-  const a = document.createElement("a");
-  a.href = url;
-  a.download = "out.txt";
-  document.body.appendChild(a);
-  a.click();
-  document.body.removeChild(a);
-  URL.revokeObjectURL(url);
-}
+// Open the scheduler
+document.getElementById('open-scheduler').addEventListener('click', () => {
+  chrome.tabs.create({ url: chrome.runtime.getURL('scheduler.html') });
+});
