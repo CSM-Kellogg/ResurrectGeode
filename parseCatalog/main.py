ef310bb76b2f5f39b36abf2266e27060f3ab7d70
LiamKellogg <kellogg@mines.edu>
2025-06-01 20:53:56 -0600

    Finished initial parse of catalog
    Uncovered some errors in searchCourses.js, which needs to be resolved at some point. Marking as issue so I don't forget

--- parseCatalog/main.py
+++ parseCatalog/main.py
@@ -33,11 +33,15 @@ while len(result) > 0:
     i = 0
     while i < len(result):
         # Check if the course name matches (exact) and the CRN is unique to the course name
-        if result[i][7] == tmp_course[7] and result[i][0] not in [j[0] for j in sections]:
-            sections.append([result[i][0]] + result[i][2:5] + result[i][12:]) # Append as a section
+        if result[i][7] == tmp_course[7]:
+            if result[i][0] not in [j[0] for j in sections]:
+                sections.append([result[i][0]] + result[i][2:5] + result[i][12:]) # Append as a section
             del result[i] # Pop the course from the list
         else: i += 1
     
     converted.append(tmp_course[5:12] + [tmp_course[1]] + sections)
 
-print(converted[20])
+# Write out using the GOATed library csv
+f = open("mergedSections.csv", 'w', encoding='utf-8', newline='')
+writer = csv.writer(f)
+writer.writerows(converted)
